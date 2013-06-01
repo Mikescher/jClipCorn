f2044482b4bc307ba5fa1e05897c1a4c315b03a9
Mike Schwörer <mailport@mikescher.de>
2013-06-01 17:54:27 +0200

    1.8 RELEASE
    Updated Version to 1.8

--- src/de/jClipCorn/Main.java
+++ src/de/jClipCorn/Main.java
@@ -14,7 +14,7 @@ import de.jClipCorn.util.LookAndFeelManager;
 
 public class Main {
 	public final static String TITLE = "jClipCorn"; //$NON-NLS-1$
-	public final static String VERSION = "1.7.3";	//$NON-NLS-1$
+	public final static String VERSION = "1.8";	//$NON-NLS-1$
 	public final static String DBVERSION = "1.6"; 	//$NON-NLS-1$
 	
 	private final static String PROPERTIES_PATH = "jClipcorn.properties"; //$NON-NLS-1$

c27c259d74b74c28842f92a29c9cc9fae9198ca9
Mike Schwörer <mailport@mikescher.de>
2013-07-18 15:22:55 +0200

    Added ToDos (Perhaps 1.9)

--- src/de/jClipCorn/Main.java
+++ src/de/jClipCorn/Main.java
@@ -75,6 +75,23 @@ public class Main {
 //TODO (2.0) ONLINE SYNCHRONISATION / Online Profiles / View Ratings + Viewed from others ... ...
 
 //TODO Shortcuts für SerienWindow
+
+//TODO [[ 1.9 FEATURES: ?? ]]
+
 //TODO Compare Series in CompareDialog
 
-//TODO What Features for 1.9 ??
+//TODO Backup Progressdialog has a non-functional abortbutton (should do nothing and not close window, perhaps open new window)
+    //- Add proper Helper class, this will become handy in other playes too (BackupManager ...)
+//TODO Add Link to, so you caj identify it "WARNING (12:36:14): Could not parse FSK (???) at de.jClipCorn.util.parser.AgeRatingParser.getMinimumAge (AgeRatingParser.java:208)"
+//TODO Add Backup Information Page (Backup Manager)
+	//- List All Backups in Backup Folder
+	//- Delete Backups
+	//- Create new Backup (short disconnect from DB)
+	//- Insert Backup (Instantly (w/o restart) would be cool, warn because you will loose current data)
+	//- Backups get an Information file [CreationDate, Name (only on Custom Backup), PreventAutoDelete, MovieCount?, CoverCount]
+	//- Show Backup Information (Things from InfoFile (or N/A on Backups b4 this version), Time until Deletion, Size)
+	//- Add Index to 2 Backups on same day (no override, Date is in Infofile)
+	//- Open Backup in Explorer
+	//- Time until next Backup
+	//=> Change connect mechanics so that you can choose were to show the progress (blocking-mainframe, or progresswindow in B-Manager)
+	//=> Cahnge disconnect mechanic so you can unload an reload a Database in mid-progress (TESTING!!)

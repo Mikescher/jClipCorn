a11fe2443b49242eb6a04560663917c4d16bc3a7
Mike Schwörer <mailport@mikescher.de>
2014-04-30 12:20:17 +0200

    1.9.2 Feature: Added 'pasteURL' && 'pasteImage' to EditCoverComponent

--- src/de/jClipCorn/gui/guiComponents/editCoverControl/EditCoverControl.java
+++ src/de/jClipCorn/gui/guiComponents/editCoverControl/EditCoverControl.java
@@ -22,21 +22,23 @@ import de.jClipCorn.gui.guiComponents.CoverLabel;
 import de.jClipCorn.gui.localization.LocaleBundle;
 import de.jClipCorn.gui.log.CCLog;
 import de.jClipCorn.util.formatter.PathFormatter;
+import de.jClipCorn.util.helper.ClipboardUtilities;
 import de.jClipCorn.util.helper.FileChooserHelper;
+import de.jClipCorn.util.helper.HTTPUtilities;
 import de.jClipCorn.util.helper.ImageUtilities;
 import de.jClipCorn.util.parser.ParseResultHandler;
 
 public class EditCoverControl extends AbstractEditCoverControl {
 	private static final long serialVersionUID = -4086336311809789696L;
-	
+
 	public final static int CTRL_WIDTH = ImageUtilities.COVER_WIDTH;
 	public final static int CTRL_HEIGHT = 288;
-	
+
 	private BufferedImage fullImage;
-	
+
 	private final ParseResultHandler owner;
 	private final JFileChooser coverFileChooser;
-	
+
 	private CoverLabel lblCover;
 	private JSplitButton btnFind;
 	private JButton btnCrop;
@@ -49,29 +51,29 @@ public class EditCoverControl extends AbstractEditCoverControl {
 	public EditCoverControl() { // For WindowBuilder
 		this(null);
 	}
-	
+
 	public EditCoverControl(ParseResultHandler handler) {
 		super();
-		
+
 		this.coverFileChooser = new JFileChooser(PathFormatter.getAbsoluteSelfDirectory());
 		this.owner = handler;
-		
+
 		coverFileChooser.setFileFilter(FileChooserHelper.createLocalFileFilter("EditCoverControl.coverFileChooser.description", "png", "bmp", "gif", "jpg", "jpeg")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-		
+
 		coverFileChooser.setDialogTitle(LocaleBundle.getString("EditCoverControl.coverFileChooser.title")); //$NON-NLS-1$
-		
+
 		initGUI();
-		
-		btnCrop.setEnabled(isCoverSet() && isCoverSet());
+
+		btnCrop.setEnabled(isEnabled() && isCoverSet());
 	}
-	
+
 	private void initGUI() {
 		setLayout(null);
-		
+
 		lblCover = new CoverLabel(false);
 		lblCover.setBounds(0, 34, ImageUtilities.COVER_WIDTH, ImageUtilities.COVER_HEIGHT);
 		add(lblCover);
-		
+
 		popupMenu = new JPopupMenu();
 		{
 			mntmFind = new JMenuItem(LocaleBundle.getString("EditCoverControl.btnFindExtended.text")); //$NON-NLS-1$
@@ -82,9 +84,9 @@ public class EditCoverControl extends AbstractEditCoverControl {
 				}
 			});
 			popupMenu.add(mntmFind);
-			
+
 			popupMenu.addSeparator();
-			
+
 			mntmOpen = new JMenuItem(LocaleBundle.getString("EditCoverControl.btnOpen.text")); //$NON-NLS-1$
 			mntmOpen.addActionListener(new ActionListener() {
 				@Override
@@ -98,7 +100,7 @@ public class EditCoverControl extends AbstractEditCoverControl {
 			mntmPasteurl.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					//TODO IMPLEMENT
+					parseURL();
 				}
 			});
 			popupMenu.add(mntmPasteurl); //
@@ -107,19 +109,19 @@ public class EditCoverControl extends AbstractEditCoverControl {
 			mntmPasteImg.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					//TODO IMPLEMENT
+					parseImage();
 				}
 			});
 			popupMenu.add(mntmPasteImg);
 		}
-		
+
 		btnFind = new JSplitButton(LocaleBundle.getString("EditCoverControl.btnFind.text")); //$NON-NLS-1$
 		btnFind.addSplitButtonActionListener(new SplitButtonActionListener() {
 			@Override
 			public void splitButtonClicked(ActionEvent arg0) {
 				// NOP
 			}
-			
+
 			@Override
 			public void buttonClicked(ActionEvent arg0) {
 				showFindCoverDialog();
@@ -128,7 +130,7 @@ public class EditCoverControl extends AbstractEditCoverControl {
 		btnFind.setPopupMenu(popupMenu);
 		btnFind.setBounds(102, 0, 80, 23);
 		add(btnFind);
-		
+
 		btnCrop = new JButton(LocaleBundle.getString("EditCoverControl.btnCrop.text")); //$NON-NLS-1$
 		btnCrop.addActionListener(new ActionListener() {
 			@Override
@@ -138,17 +140,17 @@ public class EditCoverControl extends AbstractEditCoverControl {
 		});
 		btnCrop.setBounds(0, 0, 60, 23);
 		add(btnCrop);
-		
+
 		setSize(CTRL_WIDTH, CTRL_HEIGHT);
 	}
-	
+
 	private void showChooseCoverDialog() {
 		int returnval = coverFileChooser.showOpenDialog(this);
-		
+
 		if (returnval != JFileChooser.APPROVE_OPTION) {
 			return;
 		}
-		
+
 		try {
 			BufferedImage read = ImageIO.read(coverFileChooser.getSelectedFile());
 			setCover(read);
@@ -156,29 +158,57 @@ public class EditCoverControl extends AbstractEditCoverControl {
 			CCLog.addError(e);
 		}
 	}
-	
+
 	private void showFindCoverDialog() {
 		(new FindCoverDialog(this, this, CCMovieTyp.MOVIE)).setVisible(true);
 	}
-	
+
 	private void showCropDialog() {
-		if (! isCoverSet()) return;
-		
+		if (!isCoverSet())
+			return;
+
 		(new CoverCropDialog(this, fullImage, this)).setVisible(true);
 	}
-	
+
+	private boolean parseURL() {
+		String url = ClipboardUtilities.getURL();
+
+		if (url == null)
+			return false;
+
+		BufferedImage img = HTTPUtilities.getImage(url);
+
+		if (img == null)
+			return false;
+
+		setCover(img);
+
+		return true;
+	}
+
+	private boolean parseImage() {
+		BufferedImage img = ClipboardUtilities.getImage();
+
+		if (img == null)
+			return false;
+
+		setCover(img);
+
+		return true;
+	}
+
 	public boolean isCoverSet() {
 		return fullImage != null;
 	}
-	
+
 	public BufferedImage getFullImage() {
 		return fullImage;
 	}
-	
+
 	public BufferedImage getResizedImage() {
-		if (! isCoverSet())
+		if (!isCoverSet())
 			return null;
-		
+
 		return ImageUtilities.resizeCoverImage(getFullImage());
 	}
 
@@ -191,14 +221,14 @@ public class EditCoverControl extends AbstractEditCoverControl {
 	public void setCover(BufferedImage nci) {
 		if (nci != null) {
 			this.fullImage = nci;
-			
+
 			lblCover.setIcon(new ImageIcon(ImageUtilities.resizeCoverImage(fullImage)));
 		} else {
 			this.fullImage = null;
-			
+
 			lblCover.setIcon(null);
 		}
-		
+
 		btnCrop.setEnabled(isCoverSet() && isCoverSet());
 	}
 
@@ -206,7 +236,7 @@ public class EditCoverControl extends AbstractEditCoverControl {
 	public void editingFinished(BufferedImage i) {
 		setCover(i);
 	}
-	
+
 	@Override
 	public void setEnabled(boolean e) {
 		btnCrop.setEnabled(e && isCoverSet());
---
+++ src/de/jClipCorn/util/helper/ClipboardUtilities.java
@@ -0,0 +1,61 @@
+package de.jClipCorn.util.helper;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ClipboardUtilities {
+	private static Clipboard systemClip = Toolkit.getDefaultToolkit().getSystemClipboard();
+	
+	public static String getString() {
+		Transferable transfer = systemClip.getContents( null );
+		
+		String data;
+		try {
+			data = (String) transfer.getTransferData( DataFlavor.stringFlavor );
+		} catch (UnsupportedFlavorException | IOException e) {
+			return null;
+		}
+		
+		return data;
+	}
+	
+	public static String getURL() {
+		Transferable transfer = systemClip.getContents( null );
+		
+		String data;
+		try {
+			data = (String) transfer.getTransferData( DataFlavor.stringFlavor );
+		} catch (UnsupportedFlavorException | IOException e) {
+			return null;
+		}
+		
+		try {
+			new URL(data);
+		} catch (MalformedURLException e) {
+			return null;
+		}
+		
+		return data;
+	}
+	
+	public static BufferedImage getImage() {
+		Transferable transfer = systemClip.getContents( null );
+		
+		BufferedImage image;
+		
+		try {
+			image = (BufferedImage)transfer.getTransferData(DataFlavor.imageFlavor);
+		} catch (UnsupportedFlavorException | IOException e) {
+			return null;
+		}
+		
+		return image;
+	}
+}

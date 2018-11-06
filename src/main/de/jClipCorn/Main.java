1020eb8a1c25011fe0c76f0c9d98f3005a839415
Mike Schwörer <mailport@mikescher.de>
2018-11-06 18:41:57 +0100

    1.10.6 Task: Re-add ability to do local grade beta builds

--- src/main/de/jClipCorn/Main.java
+++ src/main/de/jClipCorn/Main.java
@@ -15,13 +15,13 @@ import de.jClipCorn.util.helper.LookAndFeelManager;
 
 public class Main {
 	public final static String TITLE = "jClipCorn"; //$NON-NLS-1$
-	public final static String VERSION = /*<gradle_version_marker>*/"1.10.5"/*</gradle_version_marker>*/;	//$NON-NLS-1$
+	public final static String VERSION = /*<gradle_version_marker>*/"1.10.5.1"/*</gradle_version_marker>*/;	//$NON-NLS-1$
 	public final static String DBVERSION = "11"; 	//$NON-NLS-1$
 	
 	private final static String PROPERTIES_PATH = "jClipcorn.properties"; //$NON-NLS-1$
 	
 	public static boolean DEBUG = "true".equals(System.getProperty("ineclipse"));  //$NON-NLS-1$//$NON-NLS-2$
-	public static boolean BETA = false;
+	public static boolean BETA = true;
 		
 	public static void main(String[] arg) {
 		Globals.TIMINGS.start(Globals.TIMING_LOAD_PROPERTIES);

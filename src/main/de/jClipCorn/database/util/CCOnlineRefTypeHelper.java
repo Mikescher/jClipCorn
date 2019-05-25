2fb8ca9dee72efa3b4eb55f9442d5080f932a265
Mike Schwörer <mailport@mikescher.de>
2019-05-25 14:33:09 +0200

    1.10.7 Feature: Improved URL regexes for online references

--- src/main/de/jClipCorn/database/util/CCOnlineRefTypeHelper.java
+++ src/main/de/jClipCorn/database/util/CCOnlineRefTypeHelper.java
@@ -20,15 +20,15 @@ public final class CCOnlineRefTypeHelper {
 	public static final Pattern REGEX_KISU = Pattern.compile("^[0-9A-Za-z\\-]+$");                //$NON-NLS-1$
 	public static final Pattern REGEX_ANDB = Pattern.compile("^[0-9]+$");                         //$NON-NLS-1$
 
-	public static final Pattern REGEX_PASTE_IMDB = Pattern.compile("^(http://|https://)?(www\\.)?imdb\\.(com|de)/title/(?<id>tt[0-9]+)(/.*)?(\\?.*)?$");                                    //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_MVPT = Pattern.compile("^(http://|https://)?(www\\.)?moviepilot\\.de/(?<id>(movies|serie)/[0-9A-Za-z\\-]+)(/.*)?(\\?.*)?$");                    //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_TMDB = Pattern.compile("^(http://|https://)?(www\\.)?themoviedb\\.org/(?<id>(movie|tv)/[0-9]+)(-.*)?(/.*)?(\\?.*)?$");                          //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_MYAL = Pattern.compile("^(http://|https://)?(www\\.)?myanimelist\\.net/anime/(?<id>[0-9]+)(/.*)?(\\?.*)?$");                                    //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_PROX = Pattern.compile("^(http://|https://)?(www\\.)?proxer\\.me/info/(?<id>[0-9]+)(/.*)?(\\?.*)?$");                                           //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_ANIL = Pattern.compile("^(http://|https://)?(www\\.)?anilist\\.co/anime/(?<id>[0-9]+)(/.*)?(\\?.*)?$");                                         //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_ANPL = Pattern.compile("^(http://|https://)?(www\\.)?anime-planet\\.com/anime/(?<id>[0-9A-Za-z\\-]+)(/.*)?(\\?.*)?$");                          //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_KISU = Pattern.compile("^(http://|https://)?(www\\.)?kitsu\\.io/anime/(?<id>[0-9A-Za-z\\-]+)(/.*)?(\\?.*)?$");                                  //$NON-NLS-1$
-	public static final Pattern REGEX_PASTE_ANDB = Pattern.compile("^(http://|https://)?(www\\.)?anidb\\.net/((a)|(perl-bin/animedb\\.pl\\?show=anime&aid=))(?<id>[0-9]+)(/.*)?(\\?.*)?$"); //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_IMDB = Pattern.compile("^(http://|https://)?(www\\.)?imdb\\.(com|de)/title/(?<id>tt[0-9]+)(/.*)?(\\?.*)?(#.*)?$");                                    //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_MVPT = Pattern.compile("^(http://|https://)?(www\\.)?moviepilot\\.de/(?<id>(movies|serie)/[0-9A-Za-z\\-]+)(/.*)?(\\?.*)?(#.*)?$");                    //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_TMDB = Pattern.compile("^(http://|https://)?(www\\.)?themoviedb\\.org/(?<id>(movie|tv)/[0-9]+)(-.*)?(/.*)?(\\?.*)?(#.*)?$");                          //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_MYAL = Pattern.compile("^(http://|https://)?(www\\.)?myanimelist\\.net/anime/(?<id>[0-9]+)(/.*)?(\\?.*)?(#.*)?$");                                    //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_PROX = Pattern.compile("^(http://|https://)?(www\\.)?proxer\\.me/info/(?<id>[0-9]+)(/.*)?(\\?.*)?(#.*)?$");                                           //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_ANIL = Pattern.compile("^(http://|https://)?(www\\.)?anilist\\.co/anime/(?<id>[0-9]+)(/.*)?(\\?.*)?(#.*)?$");                                         //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_ANPL = Pattern.compile("^(http://|https://)?(www\\.)?anime-planet\\.com/anime/(?<id>[0-9A-Za-z\\-]+)(/.*)?(\\?.*)?(#.*)?$");                          //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_KISU = Pattern.compile("^(http://|https://)?(www\\.)?kitsu\\.io/anime/(?<id>[0-9A-Za-z\\-]+)(/.*)?(\\?.*)?(#.*)?$");                                  //$NON-NLS-1$
+	public static final Pattern REGEX_PASTE_ANDB = Pattern.compile("^(http://|https://)?(www\\.)?anidb\\.net/((a)|(perl-bin/animedb\\.pl\\?show=anime&aid=))(?<id>[0-9]+)(/.*)?(\\?.*)?(#.*)?$"); //$NON-NLS-1$
 
 	@SuppressWarnings("nls")
 	public static String getURL(CCSingleOnlineReference ref) {

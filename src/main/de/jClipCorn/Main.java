10c8479faa4f199a4ac59e85518796f7b5b6f858
Mike Schwörer <pubgit@mikescher.com>
2025-10-04 02:17:40 +0200

    1.10.11 RELEASE

--- src/main/de/jClipCorn/Main.java
+++ src/main/de/jClipCorn/Main.java
@@ -27,14 +27,14 @@ import de.jClipCorn.util.helper.SwingUtils;
 
 public class Main {
 	public final static String TITLE     = "jClipCorn"; //$NON-NLS-1$
-	public final static String VERSION   = /*<gradle_version_marker>*/"1.10.10.8"/*</gradle_version_marker>*/; //$NON-NLS-1$
+	public final static String VERSION   = /*<gradle_version_marker>*/"1.10.11.0"/*</gradle_version_marker>*/; //$NON-NLS-1$
 	public final static String DBVERSION = "26";    //$NON-NLS-1$
 	public final static String JXMLVER   = "10";     //$NON-NLS-1$
 
 	public final static String PROPERTIES_PATH = "jClipcorn.properties"; //$NON-NLS-1$
 	
 	public static boolean DEBUG = "true".equals(System.getProperty("ineclipse"));  //$NON-NLS-1$//$NON-NLS-2$
-	public static boolean BETA = true;
+	public static boolean BETA = false;
 
 	private static CCProperties _uiPropertyAcc;
 
